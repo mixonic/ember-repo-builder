aebf27ce150b4bbeca86e6ca9e893ad23f502752
Cory Forsyth <cory.forsyth@gmail.com>
2013-02-16 17:02:59 -0800

    make Project mirror github more closely, with repo and owner

--- lib/project.rb
+++ lib/project.rb
@@ -1,23 +1,31 @@
 class Project
-  attr_accessor :name, :git_url, :build_dir,
-    :build_task, :build_glob
+  attr_accessor :name, :build_dir,
+    :build_task, :build_glob, :repo, :owner
 
   def self.ember
     new(ember_options)
   end
 
   def initialize(options={})
-    @name         = options[:name]
-    @git_url      = options[:git_url]
+    @repo         = options[:repo]      # github repo name
+    @owner        = options[:owner]     # github owner name
     @build_dir    = options[:build_dir]
     @build_task   = options[:build_task]
     @build_glob   = options[:build_glob]
   end
 
+  def name
+    repo
+  end
+
+  def git_url
+    "https://github.com/#{owner}/#{repo}.git"
+  end
+
   def self.ember_options
     {
-      name: 'ember.js',
-      git_url: 'https://github.com/emberjs/ember.js.git',
+      repo: 'ember.js',
+      owner: 'emberjs',
       build_dir: 'dist',
       build_task: 'dist',
       is_latest: true,
